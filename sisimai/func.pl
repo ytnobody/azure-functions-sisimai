693c7bb1bc411675333f25c1dc2a740e46ef181c
ytnobody <ytnobody@gmail.com>
2017-02-22 11:48:00 +0900

    response modified

--- sisimai/func.pl
+++ sisimai/func.pl
@@ -11,8 +11,17 @@ my $file = $ENV{req};
 my $bounced = Sisimai->make($file);
 
 if (!defined $bounced) {
-    res({message => "no bounced mail is contained", bounced => []});
+    res(
+        200, 
+        {"Content-Type" => "application/json"}, 
+        {message => "no bounced mail is contained", bounced => []}
+    );
 }
 
-res({message => "found", bounced => [map {$_->damn} @$bounced]});
+res(
+    200, 
+    {"Content-Type" => "application/json"}, 
+    {message => "found", bounced => [map {$_->damn} @$bounced]}
+);
+
 
